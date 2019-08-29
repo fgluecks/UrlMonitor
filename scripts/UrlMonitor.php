3366423b4daf22ed37f7e6d35366856515f21a5b
Frederik Glücks <frederik@gluecks-online.de>
2019-08-30 00:49:32 +0200

    Changed Runtime to InvalidArgument Exception

--- scripts/UrlMonitor.php
+++ scripts/UrlMonitor.php
@@ -12,7 +12,7 @@ $Monitor = new \UrlMonitor\Monitor();
 if (isset($argv[1])) {
 	$domain = filter_var($argv[1], FILTER_SANITIZE_STRING);
 } else {
-	throw new RuntimeException("Empty Domain Parameter - Example: php UrlMonitor.php www.github.com' ");
+	throw new InvalidArgumentException("Empty Domain Parameter - Example: php UrlMonitor.php www.github.com' ");
 }
 
 $Monitor->run($domain);
