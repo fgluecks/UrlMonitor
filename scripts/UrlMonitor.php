1a8d03e489dc6a2a7dc842ffcdf537791935302f
Frederik Glücks <frederik@gluecks-online.de>
2019-08-30 00:49:59 +0200

    Changed to "use" import

--- scripts/UrlMonitor.php
+++ scripts/UrlMonitor.php
@@ -1,5 +1,7 @@
 <?php
 
+use UrlMonitor\Monitor;
+
 require_once "../libs/Tools/PublicIP.php";
 require_once "../libs/Tools/Cookies.php";
 
@@ -7,7 +9,7 @@ require_once "../libs/Output/Console.php";
 
 require_once "../libs/Monitor.php";
 
-$Monitor = new \UrlMonitor\Monitor();
+$Monitor = new Monitor();
 
 if (isset($argv[1])) {
 	$domain = filter_var($argv[1], FILTER_SANITIZE_STRING);
